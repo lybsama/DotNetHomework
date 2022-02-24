23b8811c2bf7fc48181649f0f13dc6ceed36cf1f
lybsama <1048740196@qq.com>
2022-02-25 01:31:59 +0800

    first

---
+++ Homework01/problem02/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace problem02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float x = Convert.ToSingle(textBox1.Text);
+            float y = Convert.ToSingle(textBox2.Text);
+            string a = comboBox1.Text;
+            switch (a)
+            {
+                case "+":
+                    textBox3.Text = (x + y).ToString();
+                    break;
+                case "-":
+                    textBox3.Text = (x - y).ToString();
+                    break;
+                case "*":
+                    textBox3.Text = (x * y).ToString();
+                    break;
+                case "/":
+                    textBox3.Text = (x / y).ToString();
+                    break;
+            }
+        }
+    }
+}
